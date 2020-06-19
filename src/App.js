b10f2e812cb6d4bc2547ea89d590924c9420d945
emilme <emilme@sir.mle>
2020-06-19 08:40:44 -0700

    basic formHandling setup complete

--- src/App.js
+++ src/App.js
@@ -1,10 +1,130 @@
-import React from "react";
+import React, {useState,useEffect} from "react";
+import axios from 'axios'
+import * as Yup from 'yup'
+import { Route } from 'react-router-dom';
 
+//imported files
+import scheme from './Validation/scheme'
+import Header from './Components/Header'
+import PizzaForm from './Components/PizzaForm'
+import Confirmation from './Components/Confirmation'
+
+///intial states
+const intialValues = {
+  name:'',
+  size:'',
+  toppings:{
+    Cheese: false,
+    Pineapple: false,
+    Jalapeno: false,
+    Meat: false,
+  },
+  specialInstructions:'',
+}
+
+const intialErrors = {
+  name:'',
+  size:'',
+  toppings:'',
+  specialInstructions:'',
+}
+
+const intialUser = [];
+const initialDisabled = true
+
+///APP///////////////////////////////////////
 const App = () => {
+  //setStates//
+  const [pizza, setPizza] = useState(intialUser)
+  const [formValues, setFormValues] = useState(intialValues)
+  const [error, setError] = useState(intialErrors)
+  const [disabled, setDisabled] = useState(initialDisabled)       
+  
+    ///basic form stuff////////////////////
+    const getNewPizza = ()=>{
+      axios.get('https://reqres.in/api/users', formValues)
+      .then(res=>{
+        setPizza([...pizza, res.data])
+      })
+      .catch(err=>{
+        console.log(err)
+        debugger
+      })
+    };
+  
+    const postNewPizza = (newPizza)=>{
+      axios.post('https://reqres.in/api/users', newPizza)
+      .then(res=>{
+        setPizza([...pizza, res.data])
+      })
+      .catch(err=>{
+        console.log(err)
+        debugger
+      })
+      .finally(() => {
+        setFormValues(intialValues)
+      })
+    }
+  
+    const onInputChange = evt =>{//this is where yup does it's stuff
+      const{name, value} = evt.target;
+  
+      Yup
+        .reach(scheme, name)
+        .validate(value)
+        .then(
+          setError({
+          ...error,
+          [name]:''
+          }))
+        .catch(err=>{
+          setError({
+            ...error,
+            [name]: err.errors[0]
+          })
+        })
+  
+      setFormValues({
+        ...formValues,
+        [name]: value,
+      })
+    }
+  
+    const onSubmit = evt =>{//this is where you call postNewUser
+      evt.preventDefault()
+  
+      const newUser = {
+        name:formValues.name,
+        size:formValues.size,
+        toppings:{
+          Cheese: formValues.Cheese,
+          Pineapple: formValues.Pineapple,
+          Jalapeno: formValues.Jalapeno,
+          Meat: formValues.Meat,
+        },
+        specialInstructions:formValues.specialInstructions,
+      }
+  
+      postNewUser(newUser)
+    }
   return (
-    <div>
-      Project Started.
-    </div>
+    <div className='App'>
+    <Header path='/'/>
+    <Route exact path='/pizzaform'>
+      <PizzaForm
+        values={formValues}
+        onInputChange={onInputChange}
+        onCheckboxChange={}
+        onSubmit={onSubmit}
+        disabled={disabled}
+        errors={error}
+      />
+    </Route>
+
+    <Route path='/pizzaform/pizza'>
+      <Confirmation details={pizza}/>
+    </Route>
+  </div>
   );
 };
 export default App;
