e94c95bdd44076e7bc86ca94ebbefa2a4731b0cc
emilme <emilme@sir.mle>
2020-06-19 08:44:24 -0700

    Basic form started

---
+++ src/Components/PizzaForm.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+function PizzaForm(props){
+    const {
+        values,
+        onSubmit,
+        onInputChange,
+        onCheckboxChange,
+        disabled,
+        errors,
+    } = props
+
+    return(
+        <div></div>
+    );
+}
+
+export default PizzaForm;
