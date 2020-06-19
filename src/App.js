da760901813d24f8f7d870d94cf9207f0ee1a462
emilme <emilme@sir.mle>
2020-06-19 08:11:04 -0700

    project started

--- src/App.js
+++ src/App.js
@@ -2,10 +2,9 @@ import React from "react";
 
 const App = () => {
   return (
-    <>
-      <h1>Lambda Eats</h1>
-      <p>You can remove this code and create your own header</p>
-    </>
+    <div>
+      Project Started.
+    </div>
   );
 };
 export default App;
